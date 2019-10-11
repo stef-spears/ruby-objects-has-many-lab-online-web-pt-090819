a6eda4a292df49dd4338e3a34919b64c0f9bf364
Stef Spears <spears.sj87@gmail.com>
2019-10-11 22:00:45 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Author
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    post.author = self
+  end
+  
+  def self.post_count
+    #binding.pry 
+    Post.all.count 
+  end 
+  
+end 

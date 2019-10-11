18ddd74dbda98e098d6a02528b6a1c191fde7a52
Stef Spears <spears.sj87@gmail.com>
2019-10-11 21:58:48 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,2 +1,22 @@
-class Post 
+require 'pry'
+
+class Post
+  
+  @@all = []
+  
+  attr_accessor :title, :author 
+  
+  def initialize(title)
+    @title = title 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def author_name
+    self.author ? self.author.name : nil
+  end
+  
 end 

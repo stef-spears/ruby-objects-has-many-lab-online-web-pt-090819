8f8a72f9c50a566c5965ed9ab01e595d3f39b8fd
Stef Spears <spears.sj87@gmail.com>
2019-10-11 21:51:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,21 @@ require 'pry'
 
 class Song
   
+  @@all = []
+  
   attr_accessor :name, :artist 
   
   def initialize(name)
+    @name = name 
+    @@all << self
   end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def artist_name
+    self.artist.name 
+  end
   
 end 

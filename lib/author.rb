d7dafa13adecf50ee367016b0fd9ba7c6919ec3f
Stef Spears <spears.sj87@gmail.com>
2019-10-11 22:00:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,2 +1,29 @@
-class Author 
+require 'pry'
+
+class Artist
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    post.author = self
+  end
+  
+  def self.song_count
+    #binding.pry 
+    Song.all.count 
+  end 
+  
 end 

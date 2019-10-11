3a4a5fda216f1cd881846a477e3962f68ca940a4
Stef Spears <spears.sj87@gmail.com>
2019-10-11 21:49:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,7 +9,16 @@ class Artist
   end 
   
   def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song(song)
     
-  end 
+  end
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    song.artist = self
+  end
   
 end 
